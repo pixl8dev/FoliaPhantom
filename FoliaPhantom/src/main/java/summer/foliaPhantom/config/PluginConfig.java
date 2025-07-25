621ba03c5491ceb528146a20a007df7783dd908a
summer <138532917+Summer110622@users.noreply.github.com>
2025-07-25 21:32:44 +0900

    Delete FoliaPhantom/src/main/java/summer/foliaPhantom/config directory

--- FoliaPhantom/src/main/java/summer/foliaPhantom/config/PluginConfig.java
+++
@@ -1,9 +0,0 @@
-package summer.foliaPhantom.config;
-
-public record PluginConfig(String name, String originalJarPath, String patchedJarPath, boolean foliaEnabled) {
-    public PluginConfig {
-        if (name == null || originalJarPath == null || patchedJarPath == null) {
-            throw new IllegalArgumentException("PluginConfig fields cannot be null");
-        }
-    }
-}
