bcb730dd8a8119f0e8a6fad7cc863af0d782b0cb
summer <138532917+Summer110622@users.noreply.github.com>
2025-08-15 16:49:23 +0900

    Delete folia-phantom-webapp directory

--- folia-phantom-webapp/settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-rootProject.name = "folia-phantom-3"
--- folia-phantom-webapp/src/main/java/com/marvserver/folia_phantom_3/patcher/transformers/ClassTransformer.java
+++
@@ -1,5 +0,0 @@
-package com.marvserver.folia_phantom_3.patcher.transformers;
-
-public interface ClassTransformer {
-    byte[] transform(byte[] originalBytes);
-}
